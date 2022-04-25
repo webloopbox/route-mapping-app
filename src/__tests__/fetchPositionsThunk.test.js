07ae55ea1f3c0aafe155a7abd3642070b9ae063b
Paweł <webloopbox@gmail.com>
2022-04-25 08:56:05 +0200

    add test file

--- src/__tests__/fetchPositionsThunk.test.js
+++ src/__tests__/fetchPositionsThunk.test.js
@@ -1,5 +1,38 @@
+import { fetchPositions } from "../store/searchSlice";
 
+const state = {
+    pointA: {},
+    pointB: {},
+    loading: 'false',
+    locationError: '',
+    distance: null,
+    time: null,
+    mapActive: false,
+}
 
-test('fetch positions of source/destination', () => {
+let data = { pointA: "Warsaw", pointB: "Cracow" }
+let wrongData = { pointA: "Warsaw", pointB: "asdasdasdasd" }
 
+describe('thunk', () => {
+    describe('fetch provided address', () => {
+        it('should fetch data', async () => {
+            const dispatch = jest.fn()
+            const thunk = fetchPositions(data)
+            await thunk(dispatch, () => state, undefined)
+            const { calls } = dispatch.mock
+            expect(calls[0][0].type).toEqual('search/position/pending')
+            expect(calls[1][0].type).toEqual('search/position/fulfilled')
+            console.log(dispatch.mock.calls);
+        })
+        it('should fail if wrong adress', async () => {
+            const dispatch = jest.fn()
+            const thunk = fetchPositions(wrongData)
+            await thunk(dispatch, () => state, undefined)
+            const { calls } = dispatch.mock
+            expect(calls[0][0].type).toEqual('search/position/pending')
+            expect(calls[1][0].type).toEqual('search/position/rejected')
+            expect(calls[1][0].payload).toEqual('Podano nieistniejącą lokalizację!')
+            console.log(dispatch.mock.calls);
+        })
+    })
 })
